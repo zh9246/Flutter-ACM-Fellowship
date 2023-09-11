7eafdc8f93748eff92510b9e97d03a98f99d3690
Zain Hassan <81792650+zhassan9246@users.noreply.github.com>
2023-09-11 16:57:50 +0500

    Week 7

---
+++ Week 7/app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+//  WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child:Text('hsdn') ,
+    );
+  }
+}
